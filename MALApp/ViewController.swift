835d010de81ab75f2354625da1fbb864df406fd9
Taro Altricher <ta029@hdm-stuttgart.de>
2024-03-12 01:22:10 +0100

    inital commit

---
+++ MALApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MALApp
+//
+//  Created by Taro Altrichter on 12.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
